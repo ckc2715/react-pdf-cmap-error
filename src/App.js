0e0e96f5f6e8c3e35d249b4b27fe0ea586f0d48c
Martin Chan <martin@live.hk>
2022-01-29 23:21:38 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+function App() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, ] = useState(1);
+  const [file] = useState('./article.pdf');
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document
+        file={file}
+        onLoadSuccess={onDocumentLoadSuccess}
+        options={{
+          cMapUrl: `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/cmaps/`,
+          cMapPacked: true,
+        }}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>Page {pageNumber} of {numPages}</p>
+    </div>
+  );
+}
+
+export default App;
